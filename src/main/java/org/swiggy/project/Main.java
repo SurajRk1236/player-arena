3997fb4d8fadcae1d5b24118971d8d280abcf0b5
Suraj RK <886132149r@gmail.com>
2024-06-29 16:06:54 +0530

    Player Design for playing with dies

--- src/main/java/org/swiggy/project/Main.java
+++ src/main/java/org/swiggy/project/Main.java
@@ -7,8 +7,22 @@ import org.swiggy.project.player.Player;
 
 public class Main {
     public static void main(String[] args) {
-        BasePlayer playerA = new Player(50, 5, 10);
-        BasePlayer playerB = new Player(100, 10, 5);
+        if (args.length < 6) {
+            System.out.println("Usage: java Main <healthA> <powerA> <defenseA> <healthB> <powerB> <defenseB>");
+            return;
+        }
+
+        // Parse command line arguments
+        int healthA = Integer.parseInt(args[0]);
+        int powerA = Integer.parseInt(args[1]);
+        int defenseA = Integer.parseInt(args[2]);
+        int healthB = Integer.parseInt(args[3]);
+        int powerB = Integer.parseInt(args[4]);
+        int defenseB = Integer.parseInt(args[5]);
+
+        // Create players
+        BasePlayer playerA = new Player(healthA, powerA, defenseA);
+        BasePlayer playerB = new Player(healthB, powerB, defenseB);
         Arena arena = new Arena();
         arena.fight(playerA, playerB);
     }
